e4aed8df3cf3d109acb89176b13412800e9651b0
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-03-07 14:09:28 +0300

    scritps polishing

--- toolset/mingw_gcc_x64.cmake
+++ toolset/mingw_gcc_x64.cmake
@@ -2,12 +2,15 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
+SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+SET(CMAKE_RANLIB x86_64-w64-mingw32-ranlib)
 SET(CMAKE_ASM_YASM_COMPILER yasm)
 
-SET(CMAKE_CXX_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
-SET(CMAKE_C_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
-SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
-SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+#SET(CMAKE_CXX_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+#SET(CMAKE_C_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+#SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+#SET(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "-static-libgcc -static-libstdc++ -static -O3 -s")
+
+
