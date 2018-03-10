f1390750563cbde7b02c51f2b92c3e3ca38d77e9
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-03-10 23:48:29 +0300

    update

--- install/mingw/prepare_nasm.sh
+++ install/mingw/prepare_nasm.sh
@@ -2,16 +2,19 @@
 
 set -x
 
+NASM_VER=2.13.03
 ROOT_DIR=$PWD
 WORK_DIR=$ROOT_DIR/_build_nasm
 
 mkdir -p ${WORK_DIR} && cd ${WORK_DIR} || exit 1
 
-wget --timestamping http://www.nasm.us/pub/nasm/releasebuilds/2.13.03/nasm-2.13.03.tar.xz
+wget --timestamping http://www.nasm.us/pub/nasm/releasebuilds/${NASM_VER}/nasm-${NASM_VER}.tar.xz
 
-tar -xvf nasm-2.13.03.tar.xz
+rm -fR nasm-${NASM_VER}
 
-cd nasm-2.13.03/
+tar -xvf nasm-${NASM_VER}.tar.xz
+
+cd nasm-${NASM_VER}/
 
 ./configure && make && sudo make install
 
