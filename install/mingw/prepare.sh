cceb862274f36447144a3d79aa11d6c831c93843
diamond88 <mr.aleksandr.kravchenko@gmail.com>
2018-03-10 18:42:58 +0300

    scripts

--- install/mingw/prepare.sh
+++ install/mingw/prepare.sh
@@ -7,6 +7,8 @@ sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get dist-upgrade -y; apt-
 sudo apt install -y flex texinfo bison
 sudo apt install -y build-essential
 sudo apt install -y libmpc-dev
+sudo apt install -y pkg-config
+sudo apt install -y cmake
 #sudo apt install -y autoconf
 
 ROOT_DIR=$PWD
