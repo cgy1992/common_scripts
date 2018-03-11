b1379410d1457c7a8e85817f76d2b59451771e4d
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-03-11 13:44:44 +0300

    update

--- install/install_deps.sh
+++ install/install_deps.sh
@@ -4,6 +4,7 @@ set -x
 
 sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get dist-upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
 
+sudo apt install -y wget
 sudo apt install -y flex texinfo bison
 sudo apt install -y build-essential
 sudo apt install -y libmpc-dev
